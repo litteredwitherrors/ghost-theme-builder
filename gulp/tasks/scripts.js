f06f987e15981d25937c465ac3d6b19f3ac9c62e
Dexter Lesaca <dlesaca@us.ibm.com>
2017-04-28 22:44:51 -0700

    remove node modules include

--- gulp/tasks/scripts.js
+++ gulp/tasks/scripts.js
@@ -1,13 +1,7 @@
 var gulp = require('gulp');
-var include = require('gulp-include');
 
 //Builds scripts
 gulp.task('js', () => {
   return gulp.src('./src/js/**/*.js')
-  .pipe(include({
-    includePaths: [
-      "../node_modules/analytics-platform-components/public/ap-components.min.js"
-    ]
-  }))
   .pipe(gulp.dest('./public/assets/js'));
 });
